6d5dd68a745196e59320676265753304182df3f1
wangfeng <wangfengbabe@163.com>
2017-09-09 16:34:33 +0800

    spring boot learning init

---
+++ src/main/java/FirstSpringbootExample.java
@@ -0,0 +1,22 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by wangfeng on 07/09/2017.
+ */
+@RestController
+@EnableAutoConfiguration
+public class FirstSpringbootExample {
+
+  @RequestMapping("/")
+  String home() {
+    return "hello world!";
+  }
+
+  public static void main(String[] args) {
+    SpringApplication.run(FirstSpringbootExample.class, args);
+  }
+
+}
